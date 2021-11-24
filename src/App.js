e67f6a63730686bdc16938d0a1790f3f1aabf60c
Porson <porsonlee@gmail.com>
2021-11-24 13:43:00 -0800

    press the lightswitch button will trigger dark mode or lightmode

--- src/App.js
+++ src/App.js
@@ -11,10 +11,10 @@ import TextRepeaterButton from './components/TextRepeaterButton'
 
 function App() {
   const [light, setLight] = useState('off');
-  
+  const dark = (light === "off") ? 'dark' : ""
 
   return (
-    <div className="App dark">
+    <div className={`App ${dark}`}>
       <h1>Fancy Buttons!</h1>
       <section>
         <AngryButton/>
