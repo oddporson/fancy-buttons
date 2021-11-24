61132cc5ef414728b99662f1d9a2bd14a4454b8a
Porson <porsonlee@gmail.com>
2021-11-24 14:46:00 -0800

    refactored for better parent state modifications

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ import TextRepeaterButton from './components/TextRepeaterButton'
 function App() {
   const [light, setLight] = useState('off');
   const dark = (light === "off") ? 'dark' : ""
+  const switchLight = () => setLight((light === "on") ? "off" : "on")
 
   return (
     <div className={`App ${dark}`}>
@@ -19,7 +20,7 @@ function App() {
       <section>
         <AngryButton/>
         <CounterButton/>
-        <LightSwitchButton light={light} setLight={setLight}/>
+        <LightSwitchButton light={light} switchLight={switchLight}/>
         <TextRepeaterButton/>
       </section>
     </div>
--- src/components/LightSwitchButton.js
+++ src/components/LightSwitchButton.js
@@ -1,12 +1,11 @@
 import React from "react";
 
 function LightSwitchButton(props) {
-  const {light, setLight} = props
-  // console.log(light)
-  const LightSwitchButton = () => setLight((light === "on") ? "off" : "on")
+  const {light, switchLight} = props
+  const handleClick = () => switchLight()
   
   return(
-    <button className="LightSwitchButton" onClick={LightSwitchButton}>
+    <button className="LightSwitchButton" onClick={handleClick}>
       {light === "on" && <span><i>💡</i> I'm on!</span>}
       {light === "off" && <span className="off"><i>💡</i> I'm off!</span>}
     </button>
