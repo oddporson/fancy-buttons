502cbefa8186fe10090f94656a0deccb8d20f67b
Porson <porsonlee@gmail.com>
2021-11-24 12:31:13 -0800

    updated the states of the AngerButton component. Created the onClick function to increment by 0.1 when user click the button until anger state 0 hits below 1.

--- src/components/AngryButton.js
+++ src/components/AngryButton.js
@@ -5,8 +5,15 @@ import { useState } from "react";
 
 function AngryButton() {
   const [anger, setAnger] = useState(0);
+  const handleClick = () => { 
+    if (anger < 1) {
+      setAnger(anger + 0.1)
+    } else {
+      setAnger(0);
+    }
+  }
   return (
-    <button style={{ backgroundColor: `rgba(255,0,0,${anger})` }} className="AngryButton">
+    <button style={{ backgroundColor: `rgba(255,0,0,${anger})` }} className="AngryButton" onClick={handleClick}>
       {anger < 1 && <span>Don't click me too much! </span>}
       {anger > 0 && <span>Rawr!</span>}
   </button>
