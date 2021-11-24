e531796301fdadb6cfbc60d1ffef7cccf59cf370
Porson <porsonlee@gmail.com>
2021-11-24 12:41:59 -0800

    used useState to trigger the light to turn on and off with ternary conditional

--- src/components/LightSwitchButton.js
+++ src/components/LightSwitchButton.js
@@ -4,8 +4,10 @@ import { useState } from "react";
 
 function LightSwitchButton() {
   const [light, setLight] = useState('off');
+
+  const switchLight = () => setLight((light === "on") ? "off" : "on")
   return(
-    <button className="LightSwitchButton">
+    <button className="LightSwitchButton" onClick={switchLight}>
       {light === "on" && <span><i>💡</i> I'm on!</span>}
       {light === "off" && <span className="off"><i>💡</i> I'm off!</span>}
     </button>
