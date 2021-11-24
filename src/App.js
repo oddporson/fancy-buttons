0e833784d2f0a4848d607bb04fa24b991560151c
Porson <porsonlee@gmail.com>
2021-11-24 10:51:50 -0800

    button components rendered to browser page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import './App.css';
 
+// Button Components
+import AngryButton from './components/AngryButton'
+import CounterButton from './components/CounterButton'
+import LightSwitchButton from './components/LightSwitchButton'
+import TextRepeaterButton from './components/TextRepeaterButton'
+
+
 function App() {
   return (
     <div className="App">
-      hello world.
+      <h1>Fancy Buttons!</h1>
+      <section>
+        <AngryButton/>
+        <CounterButton/>
+        <LightSwitchButton/>
+        <TextRepeaterButton/>
+      </section>
     </div>
   );
 }
