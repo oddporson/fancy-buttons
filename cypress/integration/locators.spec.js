ac5c39c1e19eb948a7a9725d4f763ab615fc642e
Porson <porsonlee@gmail.com>
2021-11-26 13:43:38 -0800

    added basic test to get element by tag name and classname for angry and lightswitch button

---
+++ cypress/integration/locators.spec.js
@@ -0,0 +1,26 @@
+/// <reference types="cypress" />
+
+describe("Locators specific elements", () => {
+
+  // before you run each test
+    beforeEach(()=> {
+      cy.visit('localhost:3000')
+    })
+  
+  it("should locate elements with GET command", ()=>{
+    // get all elements by tag name
+    cy.get("button")
+    // get element by className Angry Button
+    cy.get(".AngryButton").click()
+  })
+  it("should locate elements with GET command for LightSwitch button", ()=> {
+    cy.get("button");
+     // get element by className Angry Button
+    cy.get(".LightSwitchButton").click();
+  })
+
+
+
+
+
+})
